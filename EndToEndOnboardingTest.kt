9fafb0045e819c31c9adaa1eda596c42436e200b
Maxime ANDRÉ <maxime.andre@student.unamur.be>
2021-05-29 23:10:53 +0200

    README updated

--- EndToEndOnboardingTest.kt
+++ EndToEndOnboardingTest.kt
@@ -1,6 +1,5 @@
 package de.rki.coronawarnapp.ui
 
-import android.content.Context
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.click
@@ -8,23 +7,16 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.platform.app.InstrumentationRegistry
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.ui.main.MainActivity
 import de.rki.coronawarnapp.ui.onboarding.OnboardingActivity
 import org.hamcrest.CoreMatchers.allOf
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import java.util.Locale
 
 class EndToEndOnboardingTest {
 
@@ -71,7 +63,7 @@ class EndToEndOnboardingTest {
     }
 
     @Test
-    fun test_onboarding_navigate_privacyPageToActivationPage_withoutExposureNotificationWarning(){
+    fun test_onboarding_navigate_privacyPageToActivationPage_withoutExposureNotificationWarning() {
         // arrange
         val nxtBtn1: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // start page
         nxtBtn1.perform(click())
@@ -85,7 +77,7 @@ class EndToEndOnboardingTest {
     }
 
     @Test
-    fun test_onboarding_navigate_activationPage_activateExposure(){
+    fun test_onboarding_navigate_activationPage_activateExposure() {
         // arrange
         val nxtBtn1: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // start page
         nxtBtn1.perform(click())
@@ -93,10 +85,16 @@ class EndToEndOnboardingTest {
         val nxtBtn2: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // privacy page
         nxtBtn2.perform(click())
 
-        val warningBtn: ViewInteraction = onView(allOf(withId(android.R.id.button1), withParent(withParent(withId(R.id.buttonPanel))) )) // warning
+        val warningBtn: ViewInteraction = onView(
+            allOf(
+                withId(android.R.id.button1),
+                withParent(withParent(withId(R.id.buttonPanel)))
+            )
+        ) // warning
         warningBtn.perform(click())
 
-        val enableBtn: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // activation page
+        val enableBtn: ViewInteraction =
+            onView(withId(R.id.onboarding_button_next)) // activation page
         enableBtn.perform(click())
 
         // todo : follow instructions to allow activation; cannot be implemented using the self-signed version (which doesn't allow exposure notifications)
@@ -108,7 +106,7 @@ class EndToEndOnboardingTest {
     }
 
     @Test
-    fun test_onboarding_navigate_activationPage_deactivateExposure(){
+    fun test_onboarding_navigate_activationPage_deactivateExposure() {
         // arrange
         val nxtBtn1: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // start page
         nxtBtn1.perform(click())
@@ -116,13 +114,24 @@ class EndToEndOnboardingTest {
         val nxtBtn2: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // privacy page
         nxtBtn2.perform(click())
 
-        val warningBtn: ViewInteraction = onView(allOf(withId(android.R.id.button1), withParent(withParent(withId(R.id.buttonPanel))) )) // warning
+        val warningBtn: ViewInteraction = onView(
+            allOf(
+                withId(android.R.id.button1),
+                withParent(withParent(withId(R.id.buttonPanel)))
+            )
+        ) // warning
         warningBtn.perform(click())
 
-        val disableBtn: ViewInteraction = onView(withId(R.id.onboarding_button_disable)) // activation page
+        val disableBtn: ViewInteraction =
+            onView(withId(R.id.onboarding_button_disable)) // activation page
         disableBtn.perform(click())
 
-        val disableBtn2: ViewInteraction = onView(allOf(withId(android.R.id.button1), withParent(withParent(withId(R.id.buttonPanel))) )) // popup
+        val disableBtn2: ViewInteraction = onView(
+            allOf(
+                withId(android.R.id.button1),
+                withParent(withParent(withId(R.id.buttonPanel)))
+            )
+        ) // popup
 
         // act
         disableBtn2.perform(click())
@@ -132,7 +141,7 @@ class EndToEndOnboardingTest {
     }
 
     @Test
-    fun test_onboarding_navigate_diagnosedPageToWarningPage(){
+    fun test_onboarding_navigate_diagnosedPageToWarningPage() {
         // arrange
         val nxtBtn1: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // start page
         nxtBtn1.perform(click())
@@ -140,13 +149,24 @@ class EndToEndOnboardingTest {
         val nxtBtn2: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // privacy page
         nxtBtn2.perform(click())
 
-        val warningBtn: ViewInteraction = onView(allOf(withId(android.R.id.button1), withParent(withParent(withId(R.id.buttonPanel))) )) // warning
+        val warningBtn: ViewInteraction = onView(
+            allOf(
+                withId(android.R.id.button1),
+                withParent(withParent(withId(R.id.buttonPanel)))
+            )
+        ) // warning
         warningBtn.perform(click())
 
-        val disableBtn: ViewInteraction = onView(withId(R.id.onboarding_button_disable)) // activation page
+        val disableBtn: ViewInteraction =
+            onView(withId(R.id.onboarding_button_disable)) // activation page
         disableBtn.perform(click())
 
-        val disableBtn2: ViewInteraction = onView(allOf(withId(android.R.id.button1), withParent(withParent(withId(R.id.buttonPanel))) )) // popup
+        val disableBtn2: ViewInteraction = onView(
+            allOf(
+                withId(android.R.id.button1),
+                withParent(withParent(withId(R.id.buttonPanel)))
+            )
+        ) // popup
         disableBtn2.perform(click())
 
         val nxtBtn3: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // diagnosed page
@@ -159,7 +179,7 @@ class EndToEndOnboardingTest {
     }
 
     @Test
-    fun test_onboarding_navigate_warningPageToApplicationMainPage(){
+    fun test_onboarding_navigate_warningPageToApplicationMainPage() {
         // arrange
         val nxtBtn1: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // start page
         nxtBtn1.perform(click())
@@ -167,13 +187,24 @@ class EndToEndOnboardingTest {
         val nxtBtn2: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // privacy page
         nxtBtn2.perform(click())
 
-        val warningBtn: ViewInteraction = onView(allOf(withId(android.R.id.button1), withParent(withParent(withId(R.id.buttonPanel))) )) // warning
+        val warningBtn: ViewInteraction = onView(
+            allOf(
+                withId(android.R.id.button1),
+                withParent(withParent(withId(R.id.buttonPanel)))
+            )
+        ) // warning
         warningBtn.perform(click())
 
-        val disableBtn: ViewInteraction = onView(withId(R.id.onboarding_button_disable)) // activation page
+        val disableBtn: ViewInteraction =
+            onView(withId(R.id.onboarding_button_disable)) // activation page
         disableBtn.perform(click())
 
-        val disableBtn2: ViewInteraction = onView(allOf(withId(android.R.id.button1), withParent(withParent(withId(R.id.buttonPanel))) )) // popup
+        val disableBtn2: ViewInteraction = onView(
+            allOf(
+                withId(android.R.id.button1),
+                withParent(withParent(withId(R.id.buttonPanel)))
+            )
+        ) // popup
         disableBtn2.perform(click())
 
         val nxtBtn3: ViewInteraction = onView(withId(R.id.onboarding_button_next)) // diagnosed page
